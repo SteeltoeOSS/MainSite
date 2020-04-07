08931243bf57f8344ca80f1a7abc276fe68de407
David Dieruf <ddieruf@pivotal.io>
2020-04-07 11:22:07 -0400

    Move calendar event lookup to session init

--- src/Client/Services/Session.cs
+++ src/Client/Services/Session.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Blazored.Menu;
@@ -13,9 +14,14 @@ public class Session {
 
   public List<MenuItem> Version2_MenuItems { get; private set; }
   public List<MenuItem> Version3_MenuItems { get; private set; }
+  public CalendarEvent[] CalendarEvents { get; private set; }
 
-  public async Task LoadTOCAsync() {
+  public async Task LoadSiteItemsAsync() {
+    Debug.WriteLine("Getting menu items");
     Version2_MenuItems = await _http.GetJsonAsync<List<MenuItem>>("site-data/docs/2/toc.json");
     Version3_MenuItems = await _http.GetJsonAsync<List<MenuItem>>("site-data/docs/3/toc.json");
+    Debug.WriteLine("Version 3 count: " + Version3_MenuItems.Count);
+
+    CalendarEvents = await _http.GetJsonAsync<CalendarEvent[]>("site-data/CalendarEvents.json");
   }
 }
