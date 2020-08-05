33ea6420dc675a9d480810648d7db5d4052e3935
David Dieruf <david@dierufventures.com>
2020-08-05 13:13:52 -0400

    Added code colorizing and copy to clipboard button

--- src/Client/wwwroot/js/anchor-utils.js
+++ src/Client/wwwroot/js/anchor-utils.js
@@ -58,3 +58,23 @@ function unhov(a) {
 	if (s) { s.classList.remove(a + '-scope'); }
 	if (v) { v.classList.remove("active"); }
 }
+function highlightCode(includeCopyBadge) {
+	document.querySelectorAll('pre code').forEach((block) => { hljs.highlightBlock(block); });
+
+	if (!includeCopyBadge)
+		return;
+
+	var options = {
+		contentSelector: "#main",
+		loadDelay: 10,
+		// CSS class(es) used to render the copy icon.
+		copyIconClass: "oi oi-document",
+		// CSS class(es) used to render the done icon.
+		checkIconClass: "oi oi-check text-success",
+		// hook to allow modifying the text before it's pasted
+		onBeforeTextCopied: function (text, codeElement) {
+			return text;   //  you can fix up the text here
+		}
+	};
+	window.highlightJsBadge(options);
+}
