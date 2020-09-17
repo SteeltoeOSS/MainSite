e6362d7e0bc7be5cf0a43c199e08fac5e22319b1
David Dieruf <david@dierufventures.com>
2020-09-17 16:18:25 -0400

    Updated all projects to Blazor 3.2.1

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
 
 namespace Steeltoe.Client
 {
@@ -11,8 +12,9 @@ namespace Steeltoe.Client
 		public static async Task Main(string[] args) {
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("app");
-			
-			builder.Services.AddBaseAddressHttpClient();
+
+			//builder.Services.AddBaseAddressHttpClient();
+			builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 			builder.Services.AddSingleton<ICalendarEvents, CalendarEvents>();
 			builder.Services.AddSingleton<IParseFromMarkdown, ParseFromMarkdown>();
 			builder.Services.AddSingleton<IDocsSite, DocsSite>();
