431673885d8cce0bff3d74cfb8f5e1c17f0ef90a
Ken Swan <49496839+kenswan@users.noreply.github.com>
2021-07-27 08:46:11 -0500

    Deploy Server-side Dev Changes to Prod (#93)

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,18 @@
-FROM mcr.microsoft.com/dotnet/sdk:3.1-alpine AS build
+FROM mcr.microsoft.com/dotnet/sdk:5.0-alpine AS build
 ENV SRC_DIR=/usr/src/MainSite
 COPY src $SRC_DIR
 RUN dotnet restore $SRC_DIR \
     --runtime linux-x64
-RUN dotnet publish $SRC_DIR/Server \
+RUN dotnet publish $SRC_DIR/Client \
     --runtime linux-x64 \
-    --framework netcoreapp3.1 \
+    --framework net5.0 \
     --self-contained \
     --configuration Release \
     --output /build
 
-FROM mcr.microsoft.com/dotnet/aspnet:3.1
+FROM mcr.microsoft.com/dotnet/aspnet:5.0
 WORKDIR /app
 COPY --from=build /build .
 EXPOSE 80
-ENV ASPNETCORE_ENVIRONMENT=Staging
-ENTRYPOINT ["dotnet", "Steeltoe.Server.dll"]
+ENV ASPNETCORE_ENVIRONMENT=Production
+ENTRYPOINT ["dotnet", "Steeltoe.Client.dll"]
---
+++ src/Client/Models/DocsSiteOptions.cs
@@ -0,0 +1,27 @@
+namespace Steeltoe.Client.Models
+{
+    public class DocsSiteOptions
+    {
+        public string ApiBrowserHome { get; set; }
+        public string BaseAddress { get; set; }
+        public string BlogHome { get; set; }
+        public string DocsHome { get; set; }
+        public string DocsStreamHome { get; set; }
+        public string GuidesHome { get; set; }
+
+        public void SetUrls()
+        {
+            ApiBrowserHome = ConfigureIfMissing(ApiBrowserHome, $"{BaseAddress}/api/browser/v3/all");
+            BlogHome = ConfigureIfMissing(BlogHome, $"{BaseAddress}/articles/");
+            DocsHome = ConfigureIfMissing(DocsHome, $"{BaseAddress}/api/v3/welcome/");
+            DocsStreamHome = ConfigureIfMissing(DocsStreamHome, $"{BaseAddress}/api/v3/stream/");
+            GuidesHome = ConfigureIfMissing(GuidesHome, $"{BaseAddress}/guides/");
+        }
+
+        private static string ConfigureIfMissing(string value, string defaultValue)
+        {
+            return !string.IsNullOrWhiteSpace(value) ?
+                value : defaultValue;
+        }
+    }
+}
