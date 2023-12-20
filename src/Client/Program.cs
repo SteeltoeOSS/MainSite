670ea6f76a16192faf785197c5bd0afc2ea66449
Tim Hess <thess@vmware.com>
2023-12-20 16:48:59 -0600

    Allow any host in CORS policy

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: "DocumentationPolicy",
            policy =>
            {
-               policy.WithOrigins("http://localhost:8082", "https://docs-dev.steeltoe.io", "https://docs.steeltoe.io")
+               policy.AllowAnyOrigin()
                        .AllowAnyHeader()
                        .WithMethods("GET");
            });
