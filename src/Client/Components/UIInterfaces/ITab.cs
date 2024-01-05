68fe102932e27b609675219e890c978773a645f3
Tim Hess <tim.hess@gmail.com>
2024-01-05 17:33:54 -0600

    remove some unused components

--- src/Client/Components/UIInterfaces/ITab.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components;
-
-namespace Steeltoe.Client.Components.UIInterfaces
-{
-    public interface ITab
-    {
-        RenderFragment ChildContent { get; }
-    }
-}
